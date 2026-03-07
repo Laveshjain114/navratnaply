c8cb3e209c2b53696f2fd6fe219ba6a10f93f0f5
Laveshjain114 <luckyjain43016@gmail.com>
2026-03-07 10:43:11 +0530

    Remove superuser creation from build

--- build.sh
+++ build.sh
@@ -4,12 +4,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell -c "
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='navratnaplyadmin').exists():
-    User.objects.create_superuser('admin', 'ljain43016@gmail.com', '@Laveshjain114')
-    print('Superuser created.')
-else:
-    print('Superuser already exists.')
-"
